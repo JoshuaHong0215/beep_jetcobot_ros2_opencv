775509ea19717abab55472147263f4e076a7e183
joshua <sebin5736@gmail.com>
2026-05-28 20:26:45 +0900

    pick_place.py 작성

--- beep_jetcobot_bringup/beep_jetcobot_bringup/pick_place.py
+++ beep_jetcobot_bringup/beep_jetcobot_bringup/pick_place.py
@@ -1,5 +1,6 @@
-import rclpy as np
+import rclpy as rp
 from rclpy.node import Node
+
 
 from pymycobot.mycobot280 import MyCobot280
 import time
@@ -14,25 +15,60 @@ class PickPlaceNode(Node):
 
         
         self.home_angles = [0, 0, 0, 0, 0, 0]
+        self.pick_coords = [150.0, 0.0, 100.0, -180.0, 0.0, 90.0]
+        self.place_coords = [0.0, 150.0, 100.0, -180.0, 0.0, 90.0]
 
+        self.get_logger().info('pick_place_node 시작')
+        self.run()
 
     # home pose 정의
     def go_home(self):
-        pass
+        self.get_logger().info('홈 이동')
+        self.mc.send_angles(self.home_angles, self.speed)
+        time.sleep(3)
 
     # gripper open
+    # 숫자가 클수록 열고 작을수록 닫음
     def open_gripper(self):
-        pass
+        self.mc.set_gripper_value(100, self.speed)
+        time.sleep(1)
 
     # gripper close
     def close_gripper(self):
-        pass
+        self.mc.set_gripper_value(0, self.speed)
+        time.sleep(1)
 
     # 이동
-    def move_to(self):
-        pass
+    def move_to(self, coords, label=''):
+        self.get_logger().info(f'{label} 이동 중')
+        self.mc.send_coords(coords, self.speed, 0)
+        time.sleep(3)
+
 
     # 동작 전체 구현
     def run(self):
-        pass
+        self.get_logger().info('--- Pick and place start ---')
 
+        self.go_home()
+        self.open_gripper()
+
+        self.move_to(self.pick_coords, 'pick')
+        self.close_gripper()
+
+        # place
+        self.move_to(self.place_coords, 'place')
+        self.open_gripper()
+
+        self.go_home()
+        self.get_logger().info('--- 완료 ---')
+
+
+def main(args=None):
+    rp.init(args=args)
+    node = PickPlaceNode()
+    node.destroy_node()
+    rp.shutdown()       
+
+
+if __name__ == "__main__":
+    main()
