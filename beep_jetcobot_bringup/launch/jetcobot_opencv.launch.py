79c7ce8b5ad5d59d0cf204c4ba84e0d1d20d7dbc
JoshuaHong0215 <JoshuaHong0215>
2026-07-22 18:06:55 +0900

    jetcobot_opencv.launch.py file modified

--- beep_jetcobot_bringup/launch/jetcobot_opencv.launch.py
+++ beep_jetcobot_bringup/launch/jetcobot_opencv.launch.py
@@ -1,17 +1,8 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    urdf_path = os.path.join(
-        get_package_share_directory('beep_jetcobot_description'),
-        'urdf', 'mycobot_280_pi.urdf'
-    )
-    with open(urdf_path, 'r') as f:
-        robot_description = f.read()
-
     return LaunchDescription([
         Node(
             package='beep_jetcobot_control',
